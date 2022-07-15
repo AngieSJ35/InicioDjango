7c37558ffe049d82d73117de008220e7a6d37fc8
Angie Sibaja <lica-jasi@hotmail.com>
2022-07-15 13:12:18 -0500

    Cambios DB

--- Noticias/models.py
+++ Noticias/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Noticia(models.Model):
+	titulo = models.CharField(max_length=50)
+	descripcion = models.CharField(max_length=500)
+	autor = models.CharField(max_length=50)
